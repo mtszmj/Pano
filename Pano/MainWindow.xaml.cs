c8b3ed51102a4b6e0a731076e323bbc43fed5f4f
Mateusz <mateuszmaj@o2.pl>
2019-06-09 10:36:54 +0200

    DragDrop lists files

--- Pano/MainWindow.xaml.cs
+++ Pano/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,83 @@ namespace Pano
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private DragDropEffects EffectOk { get; } = DragDropEffects.Copy;
+        private DragDropEffects EffectNok { get; } = DragDropEffects.None;
+        private bool IsRequiredFileIncludedInDragDrop { get; set; }
+
+
+
+        private void Grid_DragEnter(object sender, DragEventArgs e)
+        {
+            MainGrid.Background = Brushes.Aquamarine;
+
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var drop = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                var directories = drop.Where(x => Directory.Exists(x));
+                var filesFromDirectories = directories.SelectMany(x => Directory.GetFiles(x, "*.jpg", SearchOption.AllDirectories));
+
+                var files = drop.Where(x => x.ToLower().EndsWith(".jpg")).Concat(filesFromDirectories);
+
+
+
+                if(files.Any())
+                {
+                    e.Handled = true;
+                    e.Effects = EffectOk;
+                    IsRequiredFileIncludedInDragDrop = true;
+
+                    foreach (var file in files)
+                    {
+                        Console.Out.WriteLine(file);
+                    }
+                }
+                else
+                {
+                    e.Handled = true;
+                    e.Effects = EffectNok;
+                    IsRequiredFileIncludedInDragDrop = false;
+                }
+            }
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+            if (IsRequiredFileIncludedInDragDrop)
+            {
+                e.Effects = EffectOk;
+            }
+            else
+            {
+                e.Effects = EffectNok;
+            }
+            
+            this.MainGrid.Background = Brushes.Red;
+        }
+
+        private void Grid_DragLeave(object sender, DragEventArgs e)
+        {
+            IsRequiredFileIncludedInDragDrop = false;
+
+            this.MainGrid.Background = Brushes.Gray;
+        }
+
+        private void MainGrid_DragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+
+            if (IsRequiredFileIncludedInDragDrop)
+            {
+                e.Effects = EffectOk;
+            }
+            else
+            {
+                e.Effects = EffectNok;
+            }
         }
     }
 }
