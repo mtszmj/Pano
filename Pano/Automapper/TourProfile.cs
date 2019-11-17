f35071420602dec1875e10335a5126423f166bbb
Mateusz <mateuszmaj@o2.pl>
2019-11-17 22:11:03 +0100

    Commit to check counting contributions after adding email to git settings

---
+++ Pano/Automapper/TourProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Pano.Model;
+using Pano.Serialization.Model;
+
+namespace Pano.Automapper
+{
+    public class TourProfile : Profile
+    {
+        public TourProfile()
+        {
+            CreateMap<TourForDb, Tour>()
+                ;
+        }
+    }
+}
