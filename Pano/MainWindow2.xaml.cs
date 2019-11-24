e7b1da4cb6dbe41abbc7ef48bd918024402300c0
Mateusz <mateuszmaj@o2.pl>
2019-11-25 00:13:22 +0100

    Material Design implementation

--- Pano/MainWindow2.xaml.cs
+++ Pano/MainWindow2.xaml.cs
@@ -1,22 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using GalaSoft.MvvmLight.Ioc;
-using GalaSoft.MvvmLight.Views;
-using Pano.Service;
 using Pano.ViewModel;
-using Pano.ViewModels;
 
 namespace Pano
 {
@@ -29,8 +15,8 @@ namespace Pano
         {
             InitializeComponent();
 
-            if(DataContext is MainViewModel vm)
-            { 
+            if (DataContext is MainViewModel vm)
+            {
                 vm.MainFrame = GlobalMainFrame;
             }
             else throw new ApplicationException("No main view model found.");
